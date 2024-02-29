f60a59e567799b889e9aad15ad3d1c510f027460
Russell Froggatt <russellfroggatt@gmail.com>
2024-02-29 12:50:05 +0000

    Update structure for IntelliJ

--- src/App.java
+++ src/App.java
@@ -6,25 +6,65 @@ class MyTableModel extends DefaultTableModel {
     public MyTableModel(int rowCount, int columnCount) {
         super(rowCount, columnCount);
     }
-
     @Override
     public boolean isCellEditable(int row, int col) {
         return false;
     }
 }
+
+class MyTable extends JTable {
+    public MyTable(TableModel tableModel) { super(tableModel); }
+    @Override
+    public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+        JComponent jc = (JComponent)super.prepareRenderer(renderer, row, column);
+        jc.setBorder(BorderFactory.createLineBorder(Color.BLACK, 4));
+        return jc;
+    }
+}
+
+class Piece {
+    public static final int EMPTY = 0;
+    public static final int NOUGHT = 1;
+    public static final int CROSS = 2;
+    public static final String[] symbol = {"", "O", "X"};
+}
+
+class Board {
+    private int[][] square = new int[3][3];
+    public int getPiece(int i, int j) {
+        return square[i][j];
+    }
+    public void setPiece(int i, int j, int piece) {
+        square[i][j] = piece;
+    }
+    public Board() {
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                square[i][j] = Piece.EMPTY;
+    }
+}
+
 public class App {
     private JFrame frame = null;
     private JTable table = null;
     private DefaultTableModel tableModel = null;
     private TableColumnModel tableColumnModel = null;
+    private Board board = null;
+    private void updateView() {
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++) {
+                tableModel.setValueAt(Piece.symbol[board.getPiece(i, j)], i, j);
+            }
+    }
     public App() {
+        board = new Board();
         frame = new JFrame("Noughts and Crosses");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        table = new JTable(tableModel = new MyTableModel(3,3));
+        table = new MyTable(tableModel = new MyTableModel(3,3));
         tableColumnModel = table.getColumnModel();
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setRowHeight(256);
-        table.setIntercellSpacing(new Dimension(8,8));
+        table.setBorder(BorderFactory.createLineBorder(Color.BLACK, 8));
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(JLabel.CENTER);
         for(int colIdx = 0; colIdx < 3; colIdx++) {
@@ -32,7 +72,7 @@ public class App {
             col.setPreferredWidth(256);
             col.setCellRenderer(centerRenderer);
         }
-
+        table.setBackground(new Color(0,128,0));
         table.setGridColor(Color.BLACK);
         table.setShowGrid(true);
         table.setCellSelectionEnabled(false);
@@ -41,8 +81,7 @@ public class App {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
+        updateView();
         frame.setVisible(true);
-        tableModel.setValueAt("O",1,1);
-        tableModel.setValueAt("X",1,2);
     }
 }
